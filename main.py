94272cf7f898cc08aaf65204c3d0b22dbe9d6326
null null <gbisgenius@gmail.com>
2020-05-25 01:04:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+age  =int(input ("Enter your age: "))
+
+if (age>18):
+  print(f'You are adult')
+elif (age >12):
+  print(f' you are teen')
+elif (age >4):
+    print (f' you are child')
+else:
+  print(f' you are toddler')
